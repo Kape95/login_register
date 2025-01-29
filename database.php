b0db671dd926bc7aab84f0b28ec830ccb94cd5d5
Kape95 <kapelecode@gmail.com>
2025-01-29 18:13:30 +0000

    premier commit

--- database.php
+++ database.php
@@ -7,11 +7,19 @@ if (isset($_POST['signup'])) {
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
-    if ($conn->query($sql) === TRUE) {
-        header("Location: login.php?signup_success=true");
+    // Vérifier si l'email existe déjà
+    $checkEmailSql = "SELECT * FROM users WHERE email='$email'";
+    $checkEmailResult = $conn->query($checkEmailSql);
+
+    if ($checkEmailResult->num_rows > 0) {
+        echo "Erreur : L'email existe déjà.";
     } else {
-        echo "Erreur : " . $conn->error;
+        $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+        if ($conn->query($sql) === TRUE) {
+            header("Location: login.php?signup_success=true");
+        } else {
+            echo "Erreur : " . $conn->error;
+        }
     }
 }
 
@@ -28,10 +36,10 @@ if (isset($_POST['login'])) {
             $_SESSION['user'] = $user;
             header("Location: index.php");
         } else {
-            echo "Mot de passe incorrect.";
+            echo "Erreur : Mot de passe incorrect.";
         }
     } else {
-        echo "Email non trouvé.";
+        echo "Erreur : Email non trouvé.";
     }
 }
 ?>
